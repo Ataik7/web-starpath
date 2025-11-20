5fe486eee9d857b58c77745b40e749d61672f837
Iván <lvt89045@gmail.com>
2025-11-20 12:56:28 +0100

    Página características completada

--- src/app/pages/feature/feature.ts
+++ src/app/pages/feature/feature.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-
+import { RouterLink } from '@angular/router'
 @Component({
   selector: 'app-feature',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './feature.html',
   styleUrl: './feature.css',
 })
